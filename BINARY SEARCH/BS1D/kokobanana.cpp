26bced806ccdfeeeee2e2fc81661099c05221a7c
Priyanshu <pr129858@gmail.com>
2024-06-21 23:34:13 +0530

    21/06/24 05

--- BINARY SEARCH/BS1D/kokobanana.cpp
+++ BINARY SEARCH/BS1D/kokobanana.cpp
@@ -1,41 +1,42 @@
 #include<bits/stdc++.h>
 using namespace std;
-int findmax(vector<int>&arr){
+int findmax(vector<int>arr){
+    int n = arr.size();
     int maxi = INT_MIN;
-    int n  = arr.size();
-    for(int i = 0; i<n;i++){
-        maxi = max(maxi ,arr[i]);
+    for(int i = 0; i<n; i++){
+        if(arr[i]>maxi){
+            maxi = arr[i];
+        }
+        maxi = max(maxi,arr[i]);
     }
     return maxi;
 }
-int calculatotalhours(vector<int>&arr,int hour){
-    int totalH =0;
-    int n = arr.size();
-    for(int i = 0; i<n;i++){
-        totalH +=ceil((double)arr[i])/(double)(hour);
-    }
-    return totalH;
+int calculatehours(vector<int>arr,int hour){
+ int n = arr.size();
+ int totalh = 0;
+ for(int i = 0; i<n; i++){
+    totalh += ceil((double)(arr[i])/(double)(hour));
 }
-int minimum(vector<int>&arr,int hour){
- int low = 1;
- int high = findmax(arr);
- while(low<=high){
-    int mid = low+(high-low)/2;
-    int totalH = calculatotalhours(arr,hour);
-    if(totalH<=hour){
-        high = mid-1;
+return totalh;
+}
+int minimum(vector<int>arr,int h){
+    int low = 1;
+    int high = findmax(arr);
+    while(low<=high){
+        int mid = low+(high-low)/2;
+        int totalH = calculatehours(arr,mid);
+        if(totalH<=h){
+            high = mid-1;
+        }
+        else{
+            low = mid+1;
+        }
     }
-    else{
-        low = mid+1;
-    }
-
- }
- return low;
+    return low;
 }
 int main(){
-    vector<int>arr = {7,11,2,3,4};
-    int hour = 8;
-    int ans = minimum(arr,hour);
-        cout<<ans<<endl;
-    
+     vector<int>arr = {7, 15, 6, 3};
+     int hours = 8;
+     int ans = minimum(arr,hours);
+     cout<<ans<<endl;
 }
