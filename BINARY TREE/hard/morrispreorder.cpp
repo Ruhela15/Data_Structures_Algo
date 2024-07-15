724dac86d1cd6196697861bcced6b1aec77df1ad
Priyanshu <pr129858@gmail.com>
2024-07-15 17:11:03 +0530

    15/07/24 05

---
+++ BINARY TREE/hard/morrispreorder.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val) : data(val), left(NULL), right(NULL) {}
+};
+
+vector<int> inorderM(Node* root) {
+    vector<int> inorder;
+    Node* cur = root;
+    while (cur != NULL) {
+        if (cur->left == NULL) {
+            inorder.push_back(cur->data);
+            cur = cur->right;
+        } else {
+            Node* prev = cur->left;
+            while (prev->right && prev->right != cur) {
+                prev = prev->right;
+            }
+            if (prev->right == NULL) {
+                prev->right = cur;
+                       inorder.push_back(cur->data);
+                cur = cur->left;
+            } else {
+                prev->right = NULL;
+         
+                cur = cur->right;
+            }
+        }
+    }
+    return inorder;
+}
+
+int main() {
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->left->right = new Node(6); // This line should be removed as it overwrites the previous line
+
+    vector<int> result = inorderM(root);
+    for (int val : result) {
+        cout << val << " ";
+    }
+    return 0;
+}
