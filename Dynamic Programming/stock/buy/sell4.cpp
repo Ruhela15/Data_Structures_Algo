1fa8966df94b1a3abc2da9c08f7a3abed27e384a
Priyanshu <pr129858@gmail.com>
2024-08-12 03:25:23 +0530

    12/08/24 05

--- Dynamic Programming/stock/buy/sell4.cpp
+++ Dynamic Programming/stock/buy/sell4.cpp
@@ -1,31 +1,35 @@
 #include <vector>
 #include <algorithm>
+#include <iostream>
+
 using namespace std;
 
 // Recursive function with memoization to compute the maximum profit
-int solve(int ind, int buy, vector<int>& prices, vector<vector<int>>& dp) {
+int solve(int ind, int buy, const vector<int>& prices, vector<vector<int>>& dp) {
     // Base case: if we're past the last day, no profit can be made
-    if (ind >= prices.size())
+    if (ind >= prices.size()) {
         return 0;
-    
+    }
+
     // Check if the result is already computed
-    if (dp[ind][buy] != -1)
+    if (dp[ind][buy] != -1) {
         return dp[ind][buy];
-    
+    }
+
     // If we're in a buying state
     if (buy == 1) {
         // We can either buy today or skip to the next day
         return dp[ind][buy] = max(-prices[ind] + solve(ind + 1, 0, prices, dp),
-                                 0 + solve(ind + 1, 1, prices, dp));
+                                 solve(ind + 1, 1, prices, dp));
     } else { // We're in a selling state
         // We can either sell today or skip to the next day
         return dp[ind][buy] = max(prices[ind] + solve(ind + 2, 1, prices, dp),
-                                 0 + solve(ind + 1, 0, prices, dp));
+                                 solve(ind + 1, 0, prices, dp));
     }
 }
 
 // Function to initialize the DP table and start the recursion
-int stockProfit(vector<int>& prices) {
+int stockProfit(const vector<int>& prices) {
     int n = prices.size();
     // Initialize DP table with -1 to indicate uncomputed states
     vector<vector<int>> dp(n, vector<int>(2, -1));
