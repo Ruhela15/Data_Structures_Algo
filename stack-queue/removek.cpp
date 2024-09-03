0fd9e94cd1e90a801757a9517fed01a16adc5670
Priyanshu <pr129858@gmail.com>
2024-09-03 09:08:17 +0530

    02/09/24 05

---
+++ stack-queue/removek.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+#include <stack>
+#include <string>
+using namespace std;
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        stack<char> st;
+        for (char digit : num) {
+            while (!st.empty() && k > 0 && st.top() > digit) {
+                st.pop();
+                k--;
+            }
+            st.push(digit);
+        }
+
+        while (k > 0 && !st.empty()) {
+            st.pop();
+            k--;
+        }
+
+        string result;
+        while (!st.empty()) {
+            result += st.top();
+            st.pop();
+        }
+        reverse(result.begin(), result.end());
+        
+        size_t pop = result.find_first_not_of('0');
+        result = (pop == string::npos) ? "0" : result.substr(pop);
+        
+        return result;
+    }
+};
